1a25a45b4cb8b2a1d347f81f3644f63c90c1cd75
minju-sin <sinminju04@gmail.com>
2024-02-08 12:45:42 +0900

    파일 읽기 readFile

--- basics/03/list-3.js
+++ basics/03/list-3.js
@@ -1,2 +1,11 @@
 // fs 모듈의 readdir 함수의 withFileTypes 옵션 연습하기 ( 결과 비교 파일 : 03\results\list-3.js)
 
+const fs = require("fs");
+
+// 파일 읽기 
+fs.readFile("./example.txt", "utf8", (err, data)=> {
+    if(err){
+        console.log(err);
+    }
+    console.log(data);
+});
